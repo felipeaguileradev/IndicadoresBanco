42ed0badf7bf582b6b2715a1bbf9858f497a1f1c
Felipe Aguilera <felipe.aguilera.dev@gmail.com>
2023-05-31 15:20:22 -0400

    eliminacion de log

--- src/api/cmfChileDB.tsx
+++ src/api/cmfChileDB.tsx
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import {API_KEY} from '@env';
 
-console.log(API_KEY);
-
 const cmfChileDB = axios.create({
   baseURL: 'http://api.cmfchile.cl/api-sbifv3/recursos_api',
   params: {
